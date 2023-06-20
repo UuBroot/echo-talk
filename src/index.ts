19a4b20e43e586617e678124bedd9e7b18ecd38c
UuBroot <lolylyt@gmail.com>
2023-06-20 10:44:45 +0200

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+const Gun = require('gun');
+
+/*******************
+* Start and Close  *
+*******************/
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+/********************
+*   Window Creation *   
+********************/
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+  
+    win.loadFile('./public/index.html')
+    win.webContents.openDevTools();
+}
